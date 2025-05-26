8c00d18545a0f20f3a92de69771a0d1349c7218b
cschot <40297097+cschot@users.noreply.github.com>
2025-05-26 03:41:31 +0200

    update

--- .devcontainer/src/google-chrome/install.sh
+++ .devcontainer/src/google-chrome/install.sh
@@ -7,7 +7,7 @@ if [ "$CHANNEL" == "dev" ]; then
 if [ "$VERSION" == "" ]; then
     URL=https://dl.google.com/linux/direct/google-chrome-$CHANNEL_current_amd64.deb
 else 
-    URL=https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-beta/google-chrome-stable_136.0.7103.113-1_amd64.deb
+    URL=https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-$CHANNEL/google-chrome-$CHANNEL_$VERSION-1_amd64.deb
     fi
 
 apt-get update
