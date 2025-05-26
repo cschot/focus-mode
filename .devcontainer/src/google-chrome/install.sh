81d95dd1ee82dfcc1e08cac8731f4e958c61869f
cschot <40297097+cschot@users.noreply.github.com>
2025-05-26 04:13:25 +0200

    km

--- .devcontainer/src/google-chrome/install.sh
+++ .devcontainer/src/google-chrome/install.sh
@@ -4,10 +4,10 @@ if [ "$CHANNEL" = "dev" ]; then
     export $CHANNEL = "unstable"
     fi
 
-if [ "$VERSION" = "" ]; then
+if [ -z "$VERSION" ]; then
     URL="https://dl.google.com/linux/direct/google-chrome-$CHANNEL_current_amd64.deb"
 else 
-    URL="https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-$CHANNEL/google-chrome-$CHANNEL_$VERSION-1_amd64.deb"
+    URL="https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-$CHANNEL/google-chrome-${CHANNEL}_$VERSION-1_amd64.deb"
     fi
 
 apt-get update
