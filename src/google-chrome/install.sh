5f22cd19c955a45646bf51a0e73f2efae1582ffa
cschot <40297097+cschot@users.noreply.github.com>
2025-05-26 03:37:58 +0200

    update

--- src/google-chrome/install.sh
+++ src/google-chrome/install.sh
@@ -1,3 +1,14 @@
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_$(dpkg --print-architecture).deb -o /tmp/chrome.deb \
-    && apt-get -y install /tmp/chrome.deb
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$CHANNEL" == "dev" ]; then
+    export $CHANNEL = "unstable"
+    fi
+
+if [ "$VERSION" == "" ]; then
+    URL=https://dl.google.com/linux/direct/google-chrome-$CHANNEL_current_amd64.deb
+else 
+    URL=https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-beta/google-chrome-stable_136.0.7103.113-1_amd64.deb
+    fi
+
+apt-get update
+apt-get -y $URL
